1ace36d908891cd380080ed2660873ed86090ade
mario <github@lxxxvi.ch>
2023-12-29 11:23:53 +0100

    update dependencies, activate new cops

--- lib/ruboclean/rubocop_configuration_path.rb
+++ lib/ruboclean/rubocop_configuration_path.rb
@@ -11,7 +11,7 @@ module Ruboclean
     # Thrown if given path is invalid
     class InvalidPathError < StandardError
       def initialize(path)
-        super "path does not exist: '#{path}'"
+        super("path does not exist: '#{path}'")
       end
     end
 
