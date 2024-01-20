05804772ddf7b461d11767d2bfb8cc9941fdd749
Issy Long <me@issyl0.co.uk>
2024-01-20 22:11:38 +0000

    More comprehensive `path_cleanup` test

--- test/ruboclean/path_cleanup_test.rb
+++ test/ruboclean/path_cleanup_test.rb
@@ -20,17 +20,31 @@ module Ruboclean
       assert_equal output, Ruboclean::PathCleanup.new(input).cleanup
     end
 
+    # rubocop:disable Metrics/MethodLength
     def test_path_cleanup_include_and_exclude
       input = {
         SomeCop: {
           Include: ["lib/**/*.rb", "test/fixtures/not_here.rb"],
-          Exclude: ["config/**/*.rb", "test/fixtures/other_file_exists.rb", "some_other_non_existent_file.rb"]
+          Exclude: ["config/**/*.rb", "test/fixtures/other_file_exists.rb", "some_other_non_existent_file.rb"],
+          EnforcedStyle: "something"
+        },
+        SomeOtherCop: {
+          EnforcedStyle: "something"
         }
       }
-      output = { SomeCop: { Include: ["lib/**/*.rb"],
-                            Exclude: ["config/**/*.rb", "test/fixtures/other_file_exists.rb"] } }
+      output = {
+        SomeCop: {
+          Include: ["lib/**/*.rb"],
+          Exclude: ["config/**/*.rb", "test/fixtures/other_file_exists.rb"],
+          EnforcedStyle: "something"
+        },
+        SomeOtherCop: {
+          EnforcedStyle: "something"
+        }
+      }
 
       assert_equal output, Ruboclean::PathCleanup.new(input).cleanup
     end
+    # rubocop:enable Metrics/MethodLength
   end
 end
