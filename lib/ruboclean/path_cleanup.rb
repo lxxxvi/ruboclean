8bc6ebbb30f52c5c0d2f62436b4dc78d01b5ecbb
mario <github@lxxxvi.ch>
2024-02-11 17:21:58 +0100

    remove Cop if all Include/Exclude were removed during path cleanup

--- lib/ruboclean/path_cleanup.rb
+++ lib/ruboclean/path_cleanup.rb
@@ -4,7 +4,9 @@ module Ruboclean
   # Cleans up any `Include` or `Exclude` paths that don't exist.
   # The `Include` and `Exclude` paths are relative to the directory
   # where the `.rubocop.yml` file is located. If a path includes a
-  # wildcard, it's assumed to be valid.
+  # regexp, it's assumed to be valid.
+  # If all entries in `Include` or `Exclude` are removed, the entire property is removed.
+  # If a Cop gets entirely truncated due to removing all `Includes` and/or `Exclude`, the Cop itself will be removed.
   class PathCleanup
     def initialize(configuration_hash, root_directory)
       @configuration_hash = configuration_hash
@@ -12,7 +14,13 @@ module Ruboclean
     end
 
     def cleanup
-      configuration_hash.transform_values(&method(:process_top_level_values))
+      configuration_hash.each_with_object({}) do |(top_level_key, top_level_value), hash|
+        result = process_top_level_value(top_level_value)
+
+        next if Array(result).empty? # No configuration keys left in the cop, remove the entire cop
+
+        hash[top_level_key] = result
+      end
     end
 
     private
@@ -28,11 +36,15 @@ module Ruboclean
     # }
     #
     # We process it further in case of a Hash.
-    def process_top_level_values(top_level_value)
+    def process_top_level_value(top_level_value)
       return top_level_value unless top_level_value.is_a?(Hash)
 
-      top_level_value.each_with_object({}) do |(cop_property_key, cop_property_value), agg|
-        agg[cop_property_key] = process_cop_property(cop_property_key, cop_property_value)
+      top_level_value.each_with_object({}) do |(cop_property_key, cop_property_value), hash|
+        result = process_cop_property(cop_property_key, cop_property_value)
+
+        next if Array(result).empty? # No entries left, will skip adding the key to the hash
+
+        hash[cop_property_key] = result
       end
     end
 
--- test/ruboclean/path_cleanup_test.rb
+++ test/ruboclean/path_cleanup_test.rb
@@ -5,52 +5,53 @@ require "ruboclean/path_cleanup"
 
 module Ruboclean
   class PathCleanupTest < BaseTest
-    def test_path_cleanup_includes
-      input = { SomeCop: { Include: ["some_other_file.rb", "file_exists.rb", "lib/**/*.rb"] } }
-      output = { SomeCop: { Include: ["file_exists.rb", "lib/**/*.rb"] } }
-
-      assert_equal output, Ruboclean::PathCleanup.new(input, root_directory).cleanup
-    end
-
-    def test_path_cleanup_excludes
-      input = { SomeCop: { Exclude: ["lib/**/*.rb", "file_exists.rb",
-                                     "some_other_non_existent_file.rb", "not_here.rb"] } }
-      output = { SomeCop: { Exclude: ["lib/**/*.rb", "file_exists.rb"] } }
-
-      assert_equal output, Ruboclean::PathCleanup.new(input, root_directory).cleanup
-    end
-
-    # rubocop:disable Metrics/MethodLength
     def test_path_cleanup_include_and_exclude
-      input = {
-        SomeCop: {
-          Include: ["lib/**/*.rb", "not_here.rb"],
-          Exclude: ["lib/**/*.rb", "file_exists.rb", "some_other_non_existent_file.rb"],
-          EnforcedStyle: "something"
-        },
-        SomeOtherCop: {
-          EnforcedStyle: "something"
-        }
-      }
-      output = {
-        SomeCop: {
-          Include: ["lib/**/*.rb"],
-          Exclude: ["lib/**/*.rb", "file_exists.rb"],
-          EnforcedStyle: "something"
-        },
-        SomeOtherCop: {
-          EnforcedStyle: "something"
-        }
-      }
-
       assert_equal output, Ruboclean::PathCleanup.new(input, root_directory).cleanup
     end
-    # rubocop:enable Metrics/MethodLength
 
     private
 
     def root_directory
       Pathname.new("test/fixtures/project_root")
     end
+
+    # rubocop:disable Metrics/MethodLength
+    def input
+      {
+        CopWithIncludeAndExclude: {
+          Include: ["bin/**/*", "lib/**/*.rb", "not_here.rb"],
+          Exclude: ["config/**/*", "lib/**/*.rb", "file_exists.rb", "some_other_non_existent_file.rb"],
+          EnforcedStyle: "something"
+        },
+        CopWithoutIncludeOrExclude: {
+          EnforcedStyle: "something"
+        },
+        CopWhereIncludeGetsEliminated: {
+          Include: ["bin/**/*"],
+          PreventsCopFromElimination: "yay!"
+        },
+        CopGetsCompletelyEliminated: {
+          Include: ["bin/**/*"],
+          Exclude: ["config/**/*"]
+        }
+      }
+    end
+
+    def output
+      {
+        CopWithIncludeAndExclude: {
+          Include: ["lib/**/*.rb"],
+          Exclude: ["lib/**/*.rb", "file_exists.rb"],
+          EnforcedStyle: "something"
+        },
+        CopWithoutIncludeOrExclude: {
+          EnforcedStyle: "something"
+        },
+        CopWhereIncludeGetsEliminated: {
+          PreventsCopFromElimination: "yay!"
+        }
+      }
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
